6758817c8618b063fb4a1e9e6894a4c1b4bf67c0
byronpantoja <byronpantoja@gmail.com>
2023-07-18 12:49:02 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+// const cafeMenu = [
+//   {
+//     name: "Black Coffee",
+//     ingredients: "Arabica House Blend",
+//     price: 3,
+//     photoName: "coffee/blackHouseBlend.jpg",
+//     soldOut: false,
+//   },
+//   {
+//     name: "Cafe latte",
+//     ingredients: "Local Milk + Espresso",
+//     price: 4,
+//     photoName: "coffee/latte.jpg",
+//     soldOut: false,
+//   },
+//   {
+//     name: "Extra Foam Latte",
+//     ingredients: "Steamed Local Milk + Whip + Espresso",
+//     price: 5,
+//     photoName: "coffee/xtraFoamLatte.jpg",
+//     soldOut: false,
+//   },
+//   {
+//     name: "Mocha Smoothie",
+//     ingredients: "Chocolate + Espresso + Local Milk",
+//     price: 7,
+//     photoName: "coffee/mochaSmoothie.jpg",
+//     soldOut: false,
+//   },
+//   {
+//     name: "Matcha Smoothie",
+//     ingredients: "Local Milk + Single Origin Matcha",
+//     price: 7,
+//     photoName: "coffee/matchaSmoothie.jpg",
+//     soldOut: true,
+//   },
+//   {
+//     name: "Mango Smoothie",
+//     ingredients: "Local Mango + Ice + a spray of lemon",
+//     price: 7,
+//     photoName: "coffee/mangoSmoothie.jpg",
+//     soldOut: false,
+//   },
+// ];
+
+function CoffeeMenu() {
+  return <h2>Coffee</h2>;
+}
+
+function App() {
+  return (
+    <>
+      <h1>Black House Coffee</h1>
+      <CoffeeMenu />
+    </>
+  );
+}
+
+export default App;
